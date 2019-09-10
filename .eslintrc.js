dd68db05d6092592b171fb6217bc529c7e94227d
himynameisdave <d@velunny.com>
2019-09-10 00:14:04 -0700

    🌙 Configure linter
    eslint-config-himynameisdave

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  extends: [
+    'himynameisdave/configurations/node'
+  ]
+}
